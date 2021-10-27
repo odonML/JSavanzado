f48b93b3da180073ef6511abd089663b61c272d1
odonML <odon_ml@outlook.com>
2021-10-26 19:49:40 -0500

    fix plantillas etiquetadas

--- plantillasEtiquetadas/js/main.js
+++ plantillasEtiquetadas/js/main.js
@@ -8,7 +8,7 @@ function myTag (arrayStrings, exp1, exp2){
     if(exp2 >= 18) old = 'mayor';
     else old = 'menor';
 
-    return `${str1}${nombre}${str2}${old}${srt3}`;
+    return `${str1}${exp1}${str2}${old}${srt3}`;
 }
 
 let salida = myTag`hola ${nombre} es ${edad} de edad`;
